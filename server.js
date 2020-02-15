4170f27337f5e48648b7979b8d0023010fcfe560
Mathew Ramirez <Mathew.J.Ramirez@gmail.com>
2020-02-15 11:45:37 -0600

    fixing server to work with mongoose

--- server.js
+++ server.js
@@ -1,53 +1,53 @@
 var express = require("express");
-var mongojs = require("mongojs");
+var logger = require("morgan");
+var mongoose = require("mongoose");
 var cheerio = require("cheerio");
 var axios = require("axios");
 
+var db = require("./models");
+
+var PORT = 3000;
+
 var app = express();
 
-var databaseUrl = "scraper";
-var collections = ["scrapedData"];
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
-var db = mongojs(databaseUrl, collections);
-db.on("error", function(error) {
-  console.log("Database Error:", error);
-});
+var MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
 app.get("/scrape", function(req, res) {
-axios.get("https://old.reddit.com/r/webdev/").then(function(response) {
+  axios.get("https://old.reddit.com/r/webdev/").then(function(response) {
+    var $ = cheerio.load(response.data);
 
-  var $ = cheerio.load(response.data);
+    $("p.title").each(function(i, element) {
+      var result = {};
+      result.title = $(element).text();
 
-  var results = [];
+      result.link = $(element)
+        .children()
+        .attr("href");
 
-  $("p.title").each(function(i, element) {
-
-    var title = $(element).text();
-
-    var link = $(element).children().attr("href");
-
-    results.push({
-      title: title,
-      link: link
-    });
-    if (title && link) {
-        // Insert the data in the scrapedData db
-        db.scrapedData.insert({
-          title: title,
-          link: link
-        },
-        function(err, inserted) {
-          if (err) {
-            // Log the error if one is encountered during the query
-            console.log(err);
-          }
-          else {
-            // Otherwise, log the inserted data
-            console.log(inserted);
-          }
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          // If an error occurred, log it
+          console.log(err);
         });
-      }
+    });
   });
-});
 
   console.log("Scrape Complete");
+});
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
 });
